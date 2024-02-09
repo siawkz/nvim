58454c00d7ac10ba2500c20088304cf78230eeb3
Kian Zhong Siaw <kianzhong.siaw@numtech.com>
2024-02-09 16:36:46 +0800

    Remove redundant csharpier

--- lua/plugins/extras/lang/csharp-custom.lua
+++ lua/plugins/extras/lang/csharp-custom.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-          opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "csharpier" })
-        end,
-      },
-    },
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.formatting.csharpier,
-      })
-    end,
-  },
-  {
     "mfussenegger/nvim-dap",
     optional = true,
     dependencies = {
