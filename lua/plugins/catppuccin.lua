bcf84f5b3d4b46f86f0b50d6fccfb7f5d2c65352
Kian Zhong Siaw <kianzhong.siaw@numtech.com>
2024-02-11 10:00:46 +0800

    Refactor catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,27 +1,9 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    -- you can do it like this with a config function
-    config = function()
-      require("catppuccin").setup({
-        integrations = {
-          flash = false,
-          hop = true,
-          mason = true,
-          neotree = true,
-          window_picker = true,
-          symbols_outline = true,
-          which_key = true,
-        },
-      })
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = function(_, opts)
-      opts.colorscheme = "catppuccin"
-    end,
-  },
+  "catppuccin/nvim",
+  lazy = true,
+  name = "catppuccin",
+  opts = function(_, opts)
+    opts.integrations.flash = false
+    opts.integrations.hop = true
+  end,
 }
