8eb5966b68b215e20f81b201e562c6fe28f6a769
Kian Zhong Siaw <kianzhong.siaw@numtech.com>
2024-08-30 22:27:07 +0800

    Add ssh_client osc 52 handling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,24 @@
 
 vim.g.autoformat = false
 vim.g.root_spec = { ".root", "lsp", { ".git", "lua" }, "cwd" }
+
+if os.getenv("SSH_CLIENT") ~= nil or os.getenv("SSH_TTY") ~= nil then
+  local function my_paste(_)
+    return function(_)
+      local content = vim.fn.getreg('"')
+      return vim.split(content, "\n")
+    end
+  end
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = my_paste("+"),
+      ["*"] = my_paste("*"),
+    },
+  }
+end
