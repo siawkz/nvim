058fc11f3856b380dd11b4a5793d9e7190bc410a
Kian Zhong Siaw <siawkz@gmail.com>
2024-11-23 06:10:57 +0000

    Use Mono Nico as dashboard hero

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,10 +1,35 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    dashboard = {
+  opts = function(_, opts)
+    -- Check if chafa is installed
+    -- If not, use header instead of terminal
+    if vim.fn.executable("chafa") == 0 then
+      opts.dashboard = {
+        preset = {
+          header = require("utils.banners").dashboard(),
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+      }
+      return
+    end
+    opts.dashboard = {
       preset = {
         header = require("utils.banners").dashboard(),
       },
-    },
-  },
+      sections = {
+        {
+          section = "terminal",
+          cmd = "chafa ~/.config/nvim/static/mononico.png --format symbols --symbols vhalf --size 60x22 --stretch; sleep .1",
+          height = 22,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+    }
+  end,
 }
