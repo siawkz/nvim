3d8c4669fae448989dd1efe16ed4b204bf01100e
Kian Zhong Siaw <kianzhong.siaw@numtech.com>
2025-02-10 08:54:29 +0800

    Fix lazy import sequence

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,12 +16,12 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "plugins" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.editor.overseer" },
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
