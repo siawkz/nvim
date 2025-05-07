ddb698a5ab4d22d2aac6a293e392861a6d298922
Kian Zhong Siaw <kianzhong.siaw@numtech.com>
2025-05-07 10:05:29 +0800

    Temp workaround for mason breaking changes

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,11 @@
 return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, { "stylua", "prettier" })
-  end,
+  {
+    "mason-org/mason.nvim",
+    version = "1.11.0",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "stylua", "prettier" })
+    end,
+  },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
