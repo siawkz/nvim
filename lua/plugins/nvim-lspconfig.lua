74a38735ec5b7a61b64026b723b7895538b8ad17
Kian Zhong Siaw <kianzhong.siaw@numtech.com>
2025-10-28 11:18:52 +0800

    remove eslint

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,15 +9,6 @@ return {
       clangd = function(_, opts)
         opts.capabilities.offsetEncoding = { "utf-16" }
       end,
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
     },
   },
 }
